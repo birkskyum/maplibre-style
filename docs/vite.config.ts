63abf4efa3755b6447a8b6933040348c63246704
Birk Skyum <birk.skyum@pm.me>
2023-04-06 01:31:21 +0200

    minute change

--- docs/vite.config.ts
+++ docs/vite.config.ts
@@ -3,7 +3,7 @@ import {defineConfig} from 'vite';
 import staticAdapter from 'solid-start-static';
 
 const config = defineConfig(({command, mode, ssrBuild}) => {
-    console.log('command', command);
+
     console.log('mode', mode);
 
     return {
