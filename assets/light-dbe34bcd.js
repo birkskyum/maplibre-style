a47ff18f0989e5ada8afd7fb6a31bf5c2c711ecf
birkskyum <74932975+birkskyum@users.noreply.github.com>
2023-04-06 08:48:58 +0000

    Deploying to gh-pages from @ birkskyum/publish-maplibre-style-spec-docs@c265cd224e69090625131538134f62e7f07a0e9e 🚀

--- assets/light-dbe34bcd.js
+++ assets/light-dbe34bcd.js
@@ -1,4 +1,4 @@
-import{g as h,a as i,i as r,c as o,t as u}from"./entry-client-815a31e8.js";import{M as c}from"./markdown-d15779de.js";import{s}from"./v8-31fa6040.js";import{I as y}from"./items-82346516.js";import"./property-8b9ac3cb.js";import"./subtitle-ff2dd8c8.js";const d=u("<div><!#><!/><!#><!/></div>",6);function S(){const l=`# Light
+import{g as h,a as i,i as r,c as o,t as u}from"./entry-client-25b5b67e.js";import{M as c}from"./markdown-c97785c0.js";import{s}from"./v8-31fa6040.js";import{I as y}from"./items-25cc94fd.js";import"./property-15e7c8eb.js";import"./subtitle-738d91e8.js";const d=u("<div><!#><!/><!#><!/></div>",6);function S(){const l=`# Light
     
 A style's \`light\` property provides a global light source for that style. Since this property is the light used to light extruded features, you will only see visible changes to your map style when modifying this property if you are using extrusions.
 
--- assets/sprite-62c3424d.js
+++ assets/sprite-62c3424d.js
@@ -1,4 +1,4 @@
-import{g as i,i as o,c as s,t as n}from"./entry-client-815a31e8.js";import{M as r}from"./markdown-d15779de.js";import{s as a}from"./v8-31fa6040.js";const p=n("<div></div>",2);function d(){const t=`
+import{g as i,i as o,c as s,t as n}from"./entry-client-25b5b67e.js";import{M as r}from"./markdown-c97785c0.js";import{s as a}from"./v8-31fa6040.js";const p=n("<div></div>",2);function d(){const t=`
 # Sprite
 
 A style's \`sprite\` property supplies a URL template for loading small images to use in rendering \`background-pattern\`, \`fill-pattern\`, \`line-pattern\`,\`fill-extrusion-pattern\` and \`icon-image\` style properties.
